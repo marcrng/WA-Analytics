609fcaaff11a34c8d858c201ebf1d3d6dbe034d0
Marc Rangelow <marcrng@uw.edu>
2020-03-02 23:58:17 -0800

    updated index

--- data.R
+++
@@ -1,8 +0,0 @@
-library("dplyr")
-
-# Global variables
-df_inc_by_loc <- read.csv("data/Income by Location.csv")
-df_seattle_wages <- read.csv(
-  "data/City_of_Seattle_Wages___Comparison_by_Gender_-_All_Job_Classifications.csv"
-  )
-df_occupations <- read.csv("data/Employment by Occupations.csv")
