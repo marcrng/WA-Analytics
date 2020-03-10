762d99ef24724349810cbb3d8a895f69183cff84
Marc Rangelow <marcrng@uw.edu>
2020-03-10 05:17:33 -0700

    revert to plotly from scatterd3

--- app/server.R
+++ app/server.R
@@ -10,28 +10,94 @@ server <- function(input, output) {
     
     output$occ_scatter <- renderScatterD3({
         
-        df_genders <- read.csv("../data/Comparison by Gender.csv", stringsAsFactors = F) 
+        df_genders <- read.csv("../data/Comparison by Gender.csv", stringsAsFactors = F)
+        df_genders <- df_genders[complete.cases(df_genders), ]
+        df_genders <- df_genders %>% 
+            filter(Jobtitle != "Grand Total")
         
-        testdata <- df_genders[complete.cases(df_genders), ] %>%
-            mutate(m_gap  = Male.Avg.Hrly.Rate - Female.Avg.Hrly.Rate) %>%
-            mutate(f_gap  = Female.Avg.Hrly.Rate - Male.Avg.Hrly.Rate) %>%
-            filter(Jobtitle != "Grand Total") %>% 
-            select(Jobtitle, Female.Avg.Hrly.Rate, Male.Avg.Hrly.Rate) %>% 
-            pivot_longer(
-                c(Female.Avg.Hrly.Rate, Male.Avg.Hrly.Rate),
-                names_to = "category", 
-                values_to = "hourly_rate"
-                ) %>%
-            arrange(hourly_rate) %>% 
-            ungroup()
-        
-        
+        data <- reactive({
+            df_genders[1:input$slider,]
+        })
         
         scatterD3(
-            data = testdata,
-            x = hourly_rate,
-            y = Jobtitle
+            x = data()[,input$x_var],
+            y = data()[,input$y_var],
+            xlab = input$x_var,
+            ylab = input$y_var,
+            col_var = data()[,input$col_var],
+            transitions = T,
+            tooltip_text = paste0(
+                "Job Title: ", data()[,"Jobtitle"],
+                "</br> Average Hourly Rate: ",
+                data()[,"Total.Avg.Hrly.Rate"], "$",
+                "</br> Total Number Employed: ",
+                data()[,"Total.No..Empl"]
+            )
         )
+    })
+    
+    
+    output$plotly <- renderPlotly({
         
+        
+        # Filter NA from dataframe
+        df_genders <- read.csv("../data/Comparison by Gender.csv", stringsAsFactors = F)
+        df_genders <- df_genders[complete.cases(df_genders), ] %>%
+            mutate(m_gap  = Male.Avg.Hrly.Rate - Female.Avg.Hrly.Rate) %>%
+            mutate(f_gap  = Female.Avg.Hrly.Rate - Male.Avg.Hrly.Rate) %>%
+            arrange_at(interp(input$arr_var)) %>%
+            filter(Jobtitle != "Grand Total")
+        
+        # Job title vs. hourly rate
+        plot_ly(
+            data = df_genders,
+            x = ~Male.Avg.Hrly.Rate,
+            y = ~ factor(Jobtitle, levels = Jobtitle),
+            name = "Men",
+            type = "scatter",
+            mode = "markers",
+            marker = list(color = "blue"),
+            height = 8000
+        ) %>%
+            add_trace(
+                x = ~Female.Avg.Hrly.Rate,
+                y = ~ factor(Jobtitle, levels = Jobtitle),
+                name = "Women",
+                type = "scatter",
+                mode = "markers",
+                marker = list(color = "pink")
+            ) %>%
+            layout(
+                title = "Wage Comparison by Gender",
+                plot_bgcolor = "transparent",
+                paper_bgcolor = "transparent",
+                font = list(color = "lightgrey"),
+                margin = list(
+                    l = 50,
+                    r = 50,
+                    b = 100,
+                    t = 50,
+                    pad = 1
+                ),
+                xaxis = list(
+                    title = "Average Hourly Rate",
+                    tickmode = "linear",
+                    showline = F,
+                    showticklabels = F,
+                    showgrid = F
+                ),
+                yaxis = list(
+                    ticks = "inside",
+                    dtick = 1,
+                    ticklen = 5,
+                    tickwidth = 2,
+                    tickcolor = toRGB("purple"),
+                    type = "category",
+                    title = "<b> Job Title <b>",
+                    tickmode = "linear",
+                    range = c(5, max(NROW(df_genders$Jobtitle))+1),
+                    gridcolor = "#e6e6e61c"
+                )
+            )
     })
 }
--- app/ui.R
+++ app/ui.R
@@ -3,52 +3,115 @@ library(dplyr)
 library(plotly)
 library(shinyWidgets)
 library(scatterD3)
+library(forcats)
 
 
 jobtitle_list <- read.csv("../data/Comparison by Gender.csv", stringsAsFactors = F)
 
 # Remove any rows with NA
 jobtitle_list <- jobtitle_list[complete.cases(jobtitle_list),] %>% 
-    select(Jobtitle)
+  select(Jobtitle)
+
 
 pg_overview <- tabPanel("Overview",# Page 1 label
                         titlePanel("Overview"),# Page 1 title
-                        fluidPage(#uiOutput('overview')
-                            includeMarkdown("overview.md")))
-
-
-pg_gender <- tabPanel("Gender",
-                      titlePanel("Gender"),
-                      sidebarLayout(
-                          
-                          # Sidebar content
-                          sidebarPanel(verticalLayout(
-                              
-                              # Arrange Picker
-                              selectInput(
-                                  inputId = "arr_var",
-                                  label = "Sort by:",
-                                  choices = c(
-                                      "High" = "high"
-                                  ),
-                                  selected = "High"
-                              )
-                          )),
-                          mainPanel(scatterD3Output(
-                              outputId = "occ_scatter",
-                              height = 8000
-                          ))
-                      )
+                        fluidPage(includeMarkdown("overview.md"))
 )
 
-pg_loc <- tabPanel("Race and Location",
-                   titlePanel("Race and Location"))
+
+pg_gender <- tabPanel(
+  "Gender",
+  titlePanel("Gender Comparisons"),
+  sidebarLayout(
+    
+    # Sidebar content
+    sidebarPanel(
+      
+      # X column selection
+      selectInput(
+        inputId = "x_var",
+        label = "X Variable:",
+        selected = "Female.Avg.Hrly.Rate",
+        choices = c(
+          "Hourly Pay - Women" = "Female.Avg.Hrly.Rate",
+          "Hourly Pay - Men" = "Male.Avg.Hrly.Rate",
+          "Total Average Hourly Pay" = "Total.Avg.Hrly.Rate",
+          "Number Employed - Women" = "No..Female.Empl",
+          "Number Employed - Men" = "No..Male.Empl",
+          "Total Employed"= "Total.No..Empl"
+        ),
+      ),
+      # Y column selection
+      selectInput(
+        inputId = "y_var",
+        label = "Y Variable:",
+        selected = "Male.Avg.Hrly.Rate",
+        choices = c(
+          "Hourly Pay - Women" = "Female.Avg.Hrly.Rate",
+          "Hourly Pay - Men" = "Male.Avg.Hrly.Rate",
+          "Total Average Hourly Pay" = "Total.Avg.Hrly.Rate",
+          "Number Employed - Women" = "No..Female.Empl",
+          "Number Employed - Men" = "No..Male.Empl",
+          "Total Employed"= "Total.No..Empl"
+        ),
+      ),
+      # Color selection
+      selectInput(
+        inputId = "col_var",
+        label = "Color Variable:",
+        selected = "No..Male.Empl",
+        choices = c(
+          "Hourly Pay - Women" = "Female.Avg.Hrly.Rate",
+          "Hourly Pay - Men" = "Male.Avg.Hrly.Rate",
+          "Total Average Hourly Pay" = "Total.Avg.Hrly.Rate",
+          "Number Employed - Women" = "No..Female.Empl",
+          "Number Employed - Men" = "No..Male.Empl",
+          "Total Employed"= "Total.No..Empl"
+        ),
+      ),
+      # Number of obs. slider
+      sliderInput(
+        "slider",
+        "Number of observations",
+        min = 10,
+        max = nrow(df_genders),
+        step = 1,
+        value = 100
+      )
+    ),
+    mainPanel(scatterD3Output(
+      outputId = "occ_scatter"
+    ))
+  )
+)
+
+pg_loc <- tabPanel(
+  "Race and Location",
+  titlePanel("Race and Location"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "arr_var",
+        label = "Sort by:",
+        choices = c(
+          "F" = "Female.Avg.Hrly.Rate",
+          "M" = "Male.Avg.Hrly.Rate"
+        ),
+        selected = "High"
+      )
+    ),
+    mainPanel(plotlyOutput(outputId = "plotly"))
+  )
+)
+
 
 pg_occ <- tabPanel("Occupations",
                    titlePanel("Occupations"))
 
-ui <- navbarPage("WA Salary Analytics",
+ui <- navbarPage(theme = "help.css",
+                 "WA Salary Analytics",
                  pg_overview,
                  pg_gender,
                  pg_loc,
-                 pg_occ)
+                 pg_occ
+)
